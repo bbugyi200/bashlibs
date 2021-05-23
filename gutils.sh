afa6a958160417a3d714be1dca45feb1f7648377
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-05-23 09:48:53 -0400

    Add scriptname to deprecated message

--- gutils.sh
+++ gutils.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source bugyi.sh
-wmsg "Sourcing gutils.sh is deprecated! Source bugyi.sh instead!"
+wmsg "Sourcing gutils.sh is deprecated! Source bugyi.sh instead! (%s)" "${SCRIPTNAME}"
